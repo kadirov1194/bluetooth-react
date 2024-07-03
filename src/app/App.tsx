757a8cb3d84dcbfc95cf8b1682d869eb499723e7
KHAN <shoxrux.qodirov@gmail.com>
2024-07-03 10:57:28 +0900

    Standart integration of MUI components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import "../css/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{ mx: 4 }}>
+          <Typography variant="h4" component={"h1"} gutterBottom>
+            Bluetooth connection
+          </Typography>
+        </Box>
+        <Button variant="contained">검섹</Button>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
